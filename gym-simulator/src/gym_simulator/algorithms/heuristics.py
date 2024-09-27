d8e02380d627312f8dc9472370d6f23b4dda1c20
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2024-09-27 23:22:54 +0530

    Implement bastract heuristic class

--- gym-simulator/src/gym_simulator/algorithms/heuristics.py
+++ gym-simulator/src/gym_simulator/algorithms/heuristics.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 from collections import deque
 
 from gym_simulator.algorithms.types import VmDto, TaskDto, VmAssignmentDto
@@ -7,129 +9,95 @@ def is_vm_suitable(vm: VmDto, task: TaskDto) -> bool:
     return vm.memory_mb >= task.req_memory_mb
 
 
-def round_robin(tasks: list[TaskDto], vms: list[VmDto]) -> list[VmAssignmentDto]:
-    """
-    Assigns tasks to Virtual Machines (VMs) using the Round Robin algorithm.
+class AbstractHeuristicScheduler(ABC):
+    def __init__(self, tasks: list[TaskDto], vms: list[VmDto]):
+        self.tasks = tasks
+        self.vms = vms
 
-    The Round Robin algorithm cycles through available VMs and assigns each task
-    to the next suitable VM in a circular manner, ensuring fair distribution of tasks
-    across the VMs.
-    """
-    vm_index = 0
+        self.task_map = {(task.workflow_id, task.id): task for task in tasks}
+        self.ready_tasks = deque([(task.workflow_id, task.id) for task in tasks if task.id == 0])
+        self.processed_tasks: set[tuple[int, int]] = set()
 
-    assignments: list[VmAssignmentDto] = []
-    for task in tasks:
-        for vm_offset in range(len(vms)):
-            check_vm_index = (vm_index + vm_offset) % len(vms)
-            if is_vm_suitable(vms[check_vm_index], task):
-                vm_index = check_vm_index
-                break
-        else:
-            raise Exception("No VM found for task")
+    @abstractmethod
+    def schedule(self) -> list[VmAssignmentDto]:
+        raise NotImplementedError
 
-        assignments.append(VmAssignmentDto(vms[vm_index].id, task.workflow_id, task.id))
-        vm_index = (vm_index + 1) % len(vms)
+    def add_child_tasks(self, task: TaskDto):
+        """Add child tasks to the ready queue if all parents are processed."""
+        for child_id in task.child_ids:
+            workflow_child_id = (task.workflow_id, child_id)
+            all_parents_processed = all(
+                (parent.workflow_id, parent.id) in self.processed_tasks
+                for parent in self.tasks
+                if parent.workflow_id == task.workflow_id and child_id in parent.child_ids
+            )
+            if all_parents_processed and workflow_child_id not in self.ready_tasks:
+                self.ready_tasks.append(workflow_child_id)
 
-    return assignments
+
+class RoundRobinScheduler(AbstractHeuristicScheduler):
+    def schedule(self) -> list[VmAssignmentDto]:
+        assignments: list[VmAssignmentDto] = []
+        vm_index = 0
+
+        while self.ready_tasks:
+            workflow_task_id = self.ready_tasks.popleft()
+            self.processed_tasks.add(workflow_task_id)
+            task = self.task_map[workflow_task_id]
+
+            while not is_vm_suitable(self.vms[vm_index], task):
+                vm_index = (vm_index + 1) % len(self.vms)
+
+            assignments.append(VmAssignmentDto(self.vms[vm_index].id, task.workflow_id, task.id))
+            vm_index = (vm_index + 1) % len(self.vms)
+
+            self.add_child_tasks(task)
+        return assignments
 
 
-def best_fit(tasks: list[TaskDto], vms: list[VmDto]) -> list[VmAssignmentDto]:
-    """
-    Assigns tasks to Virtual Machines (VMs) using the Best Fit algorithm.
+class BestFitScheduler(AbstractHeuristicScheduler):
+    def schedule(self) -> list[VmAssignmentDto]:
+        assignments: list[VmAssignmentDto] = []
+        est_vm_completion_times = [0] * len(self.vms)
+        est_task_start_times = {(task.workflow_id, task.id): 0 for task in self.tasks}
 
-    The Best Fit algorithm evaluates available VMs to allocate each task to the VM
-    that leaves the least amount of remaining capacity after the assignment.
-    It prioritizes VMs based on their available resources (cores) and estimated
-    completion times to minimize resource waste and balance the load across VMs.
-    """
+        while self.ready_tasks:
+            workflow_task_id = self.ready_tasks.popleft()
+            self.processed_tasks.add(workflow_task_id)
+            task = self.task_map[workflow_task_id]
 
-    est_vm_completion_times = [0] * len(vms)
-    est_task_start_times = {(task.workflow_id, task.id): 0 for task in tasks}
-    assignments: list[VmAssignmentDto] = []
+            best_vm_index = None
+            best_vm_allocation = float("inf")
+            for vm_index, vm in enumerate(self.vms):
+                if not is_vm_suitable(vm, task):
+                    continue
+                vm_allocation = task.req_memory_mb / vm.memory_mb
+                assert 0 <= vm_allocation <= 1, f"Invalid VM allocation: {vm_allocation}"
 
-    for task in tasks:
-        best_vm_index = None
-        best_vm_allocation = float("inf")
-        for vm_index, vm in enumerate(vms):
-            if not is_vm_suitable(vm, task):
-                continue
-            vm_allocation = task.req_memory_mb / vm.memory_mb
-            assert 0 <= vm_allocation <= 1, f"Invalid VM allocation: {vm_allocation}"
-
-            # If the current VM has a better fit, update the best VM
-            if best_vm_index is None or vm_allocation > best_vm_allocation:
-                best_vm_index = vm_index
-                best_vm_allocation = vm_allocation
-
-            # If the current VM has the same memory, check the estimated completion time
-            elif vm_allocation == best_vm_allocation:
-                if est_vm_completion_times[vm_index] < est_vm_completion_times[best_vm_index]:
+                # If the current VM has a better fit, update the best VM
+                if best_vm_index is None or vm_allocation > best_vm_allocation:
                     best_vm_index = vm_index
                     best_vm_allocation = vm_allocation
 
-        if best_vm_index is None:
-            raise Exception("No VM found for task")
+                # If the current VM has the same memory, check the estimated completion time
+                elif vm_allocation == best_vm_allocation:
+                    if est_vm_completion_times[vm_index] < est_vm_completion_times[best_vm_index]:
+                        best_vm_index = vm_index
+                        best_vm_allocation = vm_allocation
 
-        assignments.append(VmAssignmentDto(vms[best_vm_index].id, task.workflow_id, task.id))
+            if best_vm_index is None:
+                raise Exception("No VM found for task")
 
-        # Update the estimated completion times and task start times
-        est_process_time = task.length / vms[best_vm_index].cpu_speed_mips
-        est_end_time = (
-            max(est_vm_completion_times[best_vm_index], est_task_start_times[(task.workflow_id, task.id)])
-            + est_process_time
-        )
-        est_vm_completion_times[best_vm_index] = est_end_time
-        for child_id in task.child_ids:
-            est_task_start_times[(task.workflow_id, child_id)] = est_end_time
+            assignments.append(VmAssignmentDto(self.vms[best_vm_index].id, task.workflow_id, task.id))
 
-    return assignments
+            # Update the estimated completion times and task start times
+            est_process_time = task.length / self.vms[best_vm_index].cpu_speed_mips
+            est_end_time = (
+                max(est_vm_completion_times[best_vm_index], est_task_start_times[workflow_task_id]) + est_process_time
+            )
+            est_vm_completion_times[best_vm_index] = est_end_time
+            for child_id in task.child_ids:
+                est_task_start_times[(task.workflow_id, child_id)] = est_end_time
+            self.add_child_tasks(task)
 
-
-def min_min(tasks: list[TaskDto], vms: list[VmDto]) -> list[VmAssignmentDto]:
-    estimated_completion_times_of_vms = [0] * len(vms)
-    estimated_start_times_of_tasks = {(task.workflow_id, task.id): 0 for task in tasks}
-    assignments: list[VmAssignmentDto] = []
-    for task in tasks:
-        best_vm_index = None
-        best_vm_estimated_completion_time = float("inf")
-        for vm_index, vm in enumerate(vms):
-            if is_vm_suitable(vm, task):
-                estimated_start_time = estimated_start_times_of_tasks[(task.workflow_id, task.id)]
-                estimated_completion_time = (
-                    max(estimated_completion_times_of_vms[vm_index], estimated_start_time) + task.length
-                )
-                if estimated_completion_time < best_vm_estimated_completion_time:
-                    best_vm_index = vm_index
-                    best_vm_estimated_completion_time = estimated_completion_time
-
-        if best_vm_index is None:
-            raise Exception("No VM found for task")
-
-        assignments.append(VmAssignmentDto(vms[best_vm_index].id, task.workflow_id, task.id))
-        estimated_completion_times_of_vms[best_vm_index] = best_vm_estimated_completion_time
-        estimated_start_times_of_tasks[(task.workflow_id, task.id)] = best_vm_estimated_completion_time
-
-    return assignments
-
-
-def max_min(tasks: list[TaskDto], vms: list[VmDto]) -> list[VmAssignmentDto]:
-    estimated_completion_times_of_vms = [0] * len(vms)
-    assignments: list[VmAssignmentDto] = []
-    for task in tasks:
-        best_vm_index = None
-        for vm_index, vm in enumerate(vms):
-            if is_vm_suitable(vm, task):
-                estimated_completion_time = estimated_completion_times_of_vms[vm_index] + task.length
-                if (
-                    best_vm_index is None
-                    or estimated_completion_time > estimated_completion_times_of_vms[best_vm_index]
-                ):
-                    best_vm_index = vm_index
-
-        if best_vm_index is None:
-            raise Exception("No VM found for task")
-
-        assignments.append(VmAssignmentDto(vms[best_vm_index].id, task.workflow_id, task.id))
-        estimated_completion_times_of_vms[best_vm_index] += task.length
-
-    return assignments
+        return assignments
