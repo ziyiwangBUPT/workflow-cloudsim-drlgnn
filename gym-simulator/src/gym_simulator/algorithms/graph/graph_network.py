8f7506a6ae40c6593ff6ef87a50361892619384d
K.D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2024-11-12 21:45:56 +0530

    Implement gnn using GAT

--- gym-simulator/src/gym_simulator/algorithms/graph/graph_network.py
+++ gym-simulator/src/gym_simulator/algorithms/graph/graph_network.py
@@ -1,36 +1,30 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GIN
-
-from icecream import ic
-
-from torch_geometric.nn import global_mean_pool
-from torch_geometric.utils import dense_to_sparse
+import torch.nn.functional as F
+from torch_geometric.nn import GATConv
 
 
-class GraphNetwork(nn.Module):
-    def __init__(self, input_dim: int, out_dim: int, hidden_dim: int = 32):
+class GraphNetwork(torch.nn.Module):
+    def __init__(self, num_features: int, num_edge_features: int, hidden_size=32, target_size=1):
         super().__init__()
+        self.hidden_size = hidden_size
+        self.num_features = num_features
+        self.target_size = target_size
+        self.convs = [
+            GATConv(self.num_features, self.hidden_size, edge_dim=num_edge_features),
+            GATConv(self.hidden_size, self.hidden_size, edge_dim=num_edge_features),
+        ]
+        self.edge_scorer = nn.Linear(2 * self.hidden_size, self.target_size)
 
-        self.gin = GIN(
-            in_channels=input_dim,
-            hidden_channels=hidden_dim,
-            num_layers=3,
-            out_channels=out_dim,
-        )
+    def forward(self, x: torch.Tensor, edge_index: torch.Tensor, edge_attr: torch.Tensor) -> torch.Tensor:
+        for conv in self.convs[:-1]:
+            x = conv(x, edge_index, edge_attr=edge_attr)
+            x = F.relu(x)
+            x = F.dropout(x, training=self.training)
+        x = self.convs[-1](x, edge_index, edge_attr=edge_attr)
 
-    def forward(self, x: torch.Tensor, adj: torch.Tensor) -> torch.Tensor:
-        """
-        Unbatched forward.
+        src_nodes = edge_index[0]
+        dest_nodes = edge_index[1]
+        edge_embeddings = torch.cat([x[src_nodes], x[dest_nodes]], dim=1)
 
-        :param x: (num_nodes, input_dim)
-        :param adj: (num_nodes, num_nodes)
-        :return: (1, out_dim)
-        """
-
-        edge_index, _ = dense_to_sparse(adj)
-        batch = torch.zeros(adj.shape[0], dtype=torch.long)
-
-        h: torch.Tensor = self.gin(x, edge_index)
-        h_pool = global_mean_pool(h, batch)
-        return h_pool
+        return self.edge_scorer(edge_embeddings)
--- gym-simulator/src/gym_simulator/algorithms/graph/network.py
+++ gym-simulator/src/gym_simulator/algorithms/graph/network.py
@@ -1,26 +1,43 @@
 import torch
 import torch.nn as nn
+from torch_geometric.nn import GIN
 
 from icecream import ic
 
+from torch_geometric.utils import dense_to_sparse
 
-from gym_simulator.algorithms.graph.cnn_network import CnnNetwork
 from gym_simulator.algorithms.graph.graph_network import GraphNetwork
 
 
 class Network(nn.Module):
-    def __init__(self, n_jobs: int, n_machines: int, hidden_dim: int, out_dim: int):
+    def __init__(self, n_jobs: int, n_machines: int, hidden_dim: int, out_dim: int, embedding_dim: int = 8):
         super().__init__()
+        self.n_jobs = n_jobs
+        self.n_machines = n_machines
+        self.embedding_dim = embedding_dim
 
-        self.graph_network = GraphNetwork(input_dim=3, out_dim=hidden_dim)
-        self.cnn_network = CnnNetwork(height=n_jobs, width=n_machines, channels=4, out_dim=hidden_dim)
-
-        self.cat_network = nn.Sequential(
-            nn.Linear(2 * hidden_dim, 64),
+        self.job_encoder = nn.Sequential(
+            nn.Linear(3, hidden_dim),
             nn.ReLU(),
-            nn.Linear(64, 64),
+            nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
-            nn.Linear(64, out_dim),
+            nn.Linear(hidden_dim, embedding_dim),
+        )
+        self.machine_encoder = nn.Sequential(
+            nn.Linear(1, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, embedding_dim),
+        )
+        self.connection_encoder = nn.Sequential(
+            nn.Linear(1, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, embedding_dim),
+        )
+        self.graph_network = GraphNetwork(embedding_dim, embedding_dim)
+        self.mapper = nn.Sequential(
+            nn.Linear(n_jobs * n_machines, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, out_dim),
         )
 
     def forward(
@@ -40,32 +57,43 @@ class Network(nn.Module):
         :return: (1, 1)
         """
 
-        n_tasks = task_vm_time_cost.shape[0]
-        task_vm_completion_time = vm_completion_time.reshape(1, -1).expand(n_tasks, -1)
+        job_nodes = torch.arange(self.n_jobs)
+        job_nodes = job_nodes.unsqueeze(1).expand(self.n_jobs, self.n_machines)
+        machine_nodes = torch.arange(self.n_machines) + self.n_jobs
+        machine_nodes = machine_nodes.unsqueeze(0).expand(self.n_jobs, self.n_machines)
 
-        # cnn_in: (batch_size, n_tasks, n_machines, 3)
-        cnn_in = torch.cat(
-            (
-                task_vm_compatibility.unsqueeze(-1).unsqueeze(0),
-                task_vm_time_cost.unsqueeze(-1).unsqueeze(0),
-                task_vm_power_cost.unsqueeze(-1).unsqueeze(0),
-                task_vm_completion_time.unsqueeze(-1).unsqueeze(0),
-            ),
-            dim=-1,
-        )
-        # graph_in: (batch_size, n_tasks, 3)
-        graph_in = torch.cat(
-            (
-                task_state_scheduled.unsqueeze(-1),
-                task_state_ready.unsqueeze(-1),
-                task_completion_time.unsqueeze(-1),
-            ),
-            dim=-1,
-        )
-        # lin_in: (batch_size, n_machines)
-        cnn_out = self.cnn_network(cnn_in)
-        graph_out = self.graph_network(graph_in, adj)
+        edges = torch.cat([job_nodes.flatten().unsqueeze(1), machine_nodes.flatten().unsqueeze(1)], dim=1)
+        condition = task_vm_compatibility.flatten()
+        edges = edges[condition == 1]
 
-        # cocat: (batch_size, hidden_dim*3)
-        concat = torch.cat((cnn_out, graph_out), dim=-1)
-        return self.cat_network(concat)
+        edge_index, _ = dense_to_sparse(adj)
+        edge_index = torch.cat([edges.T, edge_index], dim=1)
+
+        job_features = torch.cat(
+            [
+                task_state_scheduled.unsqueeze(1),
+                task_state_ready.unsqueeze(1),
+                task_completion_time.unsqueeze(1),
+            ],
+            dim=1,
+        )
+        machine_features = vm_completion_time.unsqueeze(1)
+
+        connection_features = task_vm_time_cost.reshape(-1, 1)
+        connection_features = connection_features[condition == 1]
+
+        x_job_node = self.job_encoder(job_features)
+        x_machine_node = self.machine_encoder(machine_features)
+        x = torch.cat([x_job_node, x_machine_node])
+
+        edge_attr = self.connection_encoder(connection_features)
+        node_node_edges = edge_index.shape[1] - edge_attr.shape[0]
+        edge_attr = torch.cat([edge_attr, torch.zeros(node_node_edges, self.embedding_dim)], dim=0)
+
+        y = self.graph_network(x, edge_index=edge_index, edge_attr=edge_attr)
+        y = y[:-node_node_edges]
+
+        edge_y = torch.zeros_like(condition, dtype=torch.float32)
+        edge_y[condition == 1] = y.flatten()
+
+        return self.mapper(edge_y.unsqueeze(0)).flatten()
